9e464ddc8ac8bef926467afb256c156234c46023
Will <will-pearson@live.com>
2025-03-25 22:04:03 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-<script>
-  // Get all images and the modal
-  var images = document.querySelectorAll('.urban-category img');
-  var modal = document.getElementById("fullscreenModal");
-  var modalImg = document.getElementById("fullscreenImage");
+// Get all images and the modal
+var images = document.querySelectorAll('.urban-category img');
+var modal = document.getElementById("fullscreenModal");
+var modalImg = document.getElementById("fullscreenImage");
 
-  // Loop through each image to add click event for full-screen
-  images.forEach(function(image) {
-    image.onclick = function() {
-      modal.style.display = "block";
-      modalImg.src = this.src;
-    }
-  });
-
-  // Close the modal
-  function closeFullscreen() {
-    modal.style.display = "none";
+// Loop through each image to add click event for full-screen
+images.forEach(function(image) {
+  image.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
   }
-</script>
+});
+
+// Close the modal when clicking outside the image
+modal.onclick = function(event) {
+  if (event.target === modal) {
+    closeFullscreen();
+  }
+};
+
+// Close the modal function
+function closeFullscreen() {
+  modal.style.display = "none";
+}
